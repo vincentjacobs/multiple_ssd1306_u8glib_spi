697065907a0e4ce495d7a2cff1fdd20576cbfdf3
Vincent Jacobs <vincent@wildkamperen.nl>
2017-01-28 11:00:50 +0100

    Import

---
+++ multiple_ssd1306_u8glib_spi.ino
@@ -0,0 +1,36 @@
+#include "U8glib.h"
+
+U8GLIB_SSD1306_128X64 displayOne(13, 11, 10, 12, 8);
+U8GLIB_SSD1306_128X64 displayTwo(13, 11, 9, 12, 7);
+
+void setup() {
+}
+
+void drawDisplayOne(void) {
+  displayOne.setFont(u8g_font_unifont);
+  displayOne.setPrintPos(0, 20); 
+  displayOne.print("Hello, world!");
+}
+
+void drawDisplayTwo(void) {
+  displayTwo.setFont(u8g_font_unifont);
+  displayTwo.setPrintPos(0, 20); 
+  displayTwo.print("Hello, too!");
+}
+
+void loop(void) {
+
+  displayOne.begin();
+  displayOne.firstPage();  
+  do {
+    drawDisplayOne();
+  } while( displayOne.nextPage() );
+
+  displayTwo.begin();
+  displayTwo.firstPage();
+  do {
+    drawDisplayTwo();
+  } while (displayTwo.nextPage() );
+  
+  delay(5000);
+}
